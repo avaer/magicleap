627f934ac21b9c68258e7064e6a6cbd9221f7f8d
Avaer Kazmer <a@modules.io>
2018-03-20 04:50:02 -0700

    Update extract script

--- extract.js
+++ extract.js
@@ -1,7 +1,17 @@
 const path = require('path');
 const fs = require('fs');
 const unzip = require('unzip');
-const ws = fs.createReadStream(path.join(__dirname, 'lib.zip'))
-  .pipe(unzip.Extract({
-    path: __dirname,
-  }));
+
+const p = path.join(__dirname, 'lib.zip');
+fs.lstat(p, err => {
+  if (!err) {
+    const ws = fs.createReadStream(p)
+      .pipe(unzip.Extract({
+        path: __dirname,
+      }));
+  } else if (err.code === 'ENOENT') {
+    // nothing
+  } else {
+    throw err;
+  }
+});
