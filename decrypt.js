1e403ebe856f8d90d5e236e07e5e03e1d04c5e78
Avaer Kazmer <a@modules.io>
2018-03-21 10:43:32 -0700

    Add encrypt/decrypt logging

--- decrypt.js
+++ decrypt.js
@@ -7,7 +7,12 @@ const crypto = require('crypto');
 const key = process.env['MAGICLEAP'];
 
 if (key) {
+  console.warn('Decrypting magicleap module with MAGICLEAP environment variable);
   fs.createReadStream(path.join(__dirname, 'lib.zip.enc'))
     .pipe(crypto.createDecipher('aes-256-cbc', new Buffer(key, 'base64')))
     .pipe(fs.createWriteStream(path.join(__dirname, 'lib.zip')));
+  process.exit(0);
+} else {
+  console.warn('NOT decrypting magicleap module because MAGICLEAP environment variable not defined);
+  process.exit(1);
 }
